a94b615e8313cd82483207a04e79b4b8fd138e30
JIYEON <publicjy324@gmail.com>
2023-09-16 12:13:12 +0900

    정수론 - 소수 구하기

---
+++ src/main/java/com/example/algorithm/decimal/소수구하기.java
@@ -0,0 +1,41 @@
+package com.example.algorithm.decimal;
+
+import java.util.Scanner;
+
+public class 소수구하기 {
+
+    /**
+     * 소수는 에라토스테네스의 체를 이용
+     * */
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int M = Integer.parseInt(sc.nextLine());
+        int N = Integer.parseInt(sc.nextLine());
+
+        int[] arr = new int[N+1];
+
+        for(int i=1; i<arr.length; i++){
+            arr[i] = i;
+        }
+
+        for(int i=2; (i*i)<=N; i++){
+
+            if(arr[i] == 0) continue;
+
+            for(int j=i+i; j<=N; j = j+i){
+                arr[j] = 0;
+            }
+
+        }
+
+        for(int i=M; i< arr.length; i++){
+            if(arr[i] == 0 ) continue;
+            System.out.println(arr[i]);
+        }
+
+    }
+}
